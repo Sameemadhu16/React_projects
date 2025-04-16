f5d7682ea01c1eeead88b11fc817b699f51ac96b
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-04-17 00:03:37 +0530

    Add WelcomeMessage component and update App structure to include it

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,13 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
+
+    //JSX expressions must have one parent element
+    //This is why we wrap everything in a div
+    
   <div>
-    <Header/>
+    {/* <Header/>
     <Main/>
-    <Footer/>
+    <Footer/> */}
+    <WelcomeMessage/>
   </div>
   );
 }
