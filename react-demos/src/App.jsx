b67b216fe847d939ecf0e054e687278350b93cb5
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-07-29 20:21:48 +0530

    Specifying a fallback default value

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,22 +1,17 @@
 import { createContext, useContext, useState } from 'react';
 
-const ThemeContext = createContext(null);
+const ThemeContext = createContext('light');
 
 export default function MyApp() {
   const [theme, setTheme] = useState('light')
   return (
     <ThemeContext.Provider value={theme}>
       <Form />
-      <label >
-        <input 
-        type="checkbox" 
-        checked={theme === 'dark'}
-        onChange={(e) =>{
-          setTheme(e.target.checked ? 'dark' : 'light')
-        }}
-        />
-        Use Dark Mode
-      </label>
+      <Button onClick={() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+      }}>
+        Toggle Theme
+      </Button>
     </ThemeContext.Provider>
   )
 }
@@ -41,11 +36,11 @@ function Panel({ title, children }) {
   )
 }
 
-function Button({ children }) {
+function Button({ children, onClick }) {
   const theme = useContext(ThemeContext);
   const className = 'button-' + theme;
   return (
-    <button className={className}>
+    <button className={className} onClick={onClick}>
       {children}
     </button>
   );
