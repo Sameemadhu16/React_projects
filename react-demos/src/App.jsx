4d52388bff6a68f05002740c4f1690602e378e23
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-07-29 20:14:06 +0530

    Updating a value via context

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,11 +1,22 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const ThemeContext = createContext(null);
 
 export default function MyApp() {
+  const [theme, setTheme] = useState('light')
   return (
-    <ThemeContext.Provider value="dark">
+    <ThemeContext.Provider value={theme}>
       <Form />
+      <label >
+        <input 
+        type="checkbox" 
+        checked={theme === 'dark'}
+        onChange={(e) =>{
+          setTheme(e.target.checked ? 'dark' : 'light')
+        }}
+        />
+        Use Dark Mode
+      </label>
     </ThemeContext.Provider>
   )
 }
